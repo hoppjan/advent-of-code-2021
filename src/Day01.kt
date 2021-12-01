8cace21c2ca06abd8a2f521ecece1948839f2b64
Jan Hopp <jan.hopp@securepoint.de>
2021-12-01 13:16:58 +0100

    Solved day 01.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,63 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    /**
+     * Counts the increases from the given input measurements from one to another.
+     */
+    fun part1(input: List<Int>) = input.countIncreases()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    /**
+     * Counts the increases from the given input measurements from the one triplet sum to another.
+     */
+    fun part2(input: List<Int>) = part1(input.addTriplets())
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // test if implementation meets criteria from the description:
+    val testInput = readInputInts("Day01_test")
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    // testing part 1
+    val testSolution1 = 7
+    val testOutput1 = part1(testInput)
+
+    printTestResult(1, testOutput1, testSolution1)
+    check(testOutput1 == testSolution1)
+
+    // testing part 2
+    val testSolution2 = 5
+    val testOutput2 = part2(testInput)
+
+    printTestResult(2, testOutput2, testSolution2)
+    check(testOutput2 == testSolution2)
+
+    // actual input
+    val input = readInputInts("Day01")
+    printResult(1, part1(input))
+    printResult(2, part2(input))
 }
+
+/**
+ * Does invokes [onIncrease] every time the next number is bigger than its predecessor.
+ */
+private fun List<Int>.forEachIncrease(onIncrease: () -> Unit) {
+    for (i in 0 until lastIndex)
+        if (get(i) < get(i + 1))
+            onIncrease()
+}
+
+/**
+ * Counts the increases of a [List<Int>] from one number to the next.
+ */
+private fun List<Int>.countIncreases(): Int {
+    var increases = 0
+    forEachIncrease {
+        increases++
+    }
+    return increases
+}
+
+/**
+ * Adds up element and its following two neighbors as long as the two following neighbors exist.
+ */
+private fun List<Int>.addTriplets(): List<Int> =
+    mutableListOf<Int>().let { triplets ->
+        for (i in 0..lastIndex - 2)
+            triplets.add(get(i) + get(i + 1) + get(i + 2))
+        triplets
+    }
--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,23 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Converts content of the given text file to List<Int>.
+ */
+fun readInputInts(name: String) = readInput(name).map { it.toInt() }
+
+/**
+ * Prints test result of a part and solution.
+ */
+fun <T> printTestResult(part: Int = 1, result: T, expected: T) =
+    println("test result $part: $result, $expected expected")
+
+/**
+ * Prints result of a part.
+ */
+fun <T> printResult(part: Int, result: T) =
+    println("result part $part: $result")
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
