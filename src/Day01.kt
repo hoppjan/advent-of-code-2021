ed24ed5280a088afcb580c38b2e02355f3f54348
Jan Hopp <jan.hopp@securepoint.de>
2021-12-05 15:46:42 +0100

    Simplified day 01 solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,37 +7,12 @@ fun main() {
     /**
      * Counts the increases from the given input measurements from the one triplet sum to another.
      */
-    fun part2(input: List<Int>) = input.addTriplets().countIncreases()
+    fun part2(input: List<Int>) = input.countIncreases(groupSize = 3)
 
     runEverything("01", 7, 5, ::part1, ::part2, IntInputReader)
 }
 
-/**
- * Invokes [onIncrease] every time the next number is bigger than its predecessor.
- */
-private fun List<Int>.forEachIncrease(onIncrease: () -> Unit) {
-    for (i in 0 until lastIndex)
-        if (get(i) < get(i + 1))
-            onIncrease()
-}
-
-/**
- * Counts the increases of a [List<Int>] from one number to the next.
- */
-private fun List<Int>.countIncreases(): Int {
-    var increases = 0
-    forEachIncrease {
-        increases++
-    }
-    return increases
-}
-
-/**
- * Adds up element and its following two neighbors as long as the two following neighbors exist.
- */
-private fun List<Int>.addTriplets(): List<Int> =
-    mutableListOf<Int>().let { triplets ->
-        for (i in 0..lastIndex - 2)
-            triplets.add(get(i) + get(i + 1) + get(i + 2))
-        triplets
-    }
+private fun List<Int>.countIncreases(groupSize: Int = 1) =
+    windowed(groupSize + 1, 1) {
+        it.first() < it.last()
+    }.filter { it }.size
